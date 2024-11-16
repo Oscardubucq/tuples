26392d919f26085aaa1aa62c0ef7d8dc5c538961
Oscardubucq <oscar.dubucq@gmail.com>
2024-11-16 13:02:44 +0100

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,18 @@
+oscar = ("orange","banana","apple","kiwi","tangerine")
+print (oscar)
+
+# unpacking
+
+data = "oscar",14,"Luxembourg","Capellen"
+
+name , age , country , city = data
+print (country)
+
+# repetition
+
+
+# concatenation
+
+
+#length of the tuple
+
